4600e8e162ee6a917c991cc29d2395991a9236e1
Cheney Lin <lincheney@gmail.com>
2017-06-13 00:09:13 +1000

    turn the validator into a command line tool

--- validate.go
+++ validate.go
@@ -17,97 +17,49 @@ package main
 
 import (
 	"bytes"
-	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
-	"net/http"
-	"runtime/debug"
+	"os"
 
 	"github.com/coreos/coreos-cloudinit/config/validate"
 	ignConfig "github.com/coreos/ignition/config"
-	"github.com/crawford/nap"
-	"github.com/gorilla/mux"
 )
-
-var (
-	flags = struct {
-		port    int
-		address string
-	}{}
-)
-
-type payloadWrapper struct{}
-
-func (w payloadWrapper) Wrap(payload interface{}, status nap.Status) (interface{}, int) {
-	return map[string]interface{}{
-		"result": payload,
-	}, status.Code()
-}
-
-type panicHandler struct{}
-
-func (h panicHandler) Handle(e interface{}) {
-	log.Printf("PANIC: %#v\n", e)
-	debug.PrintStack()
-}
-
-func init() {
-	flag.StringVar(&flags.address, "address", "0.0.0.0", "address to listen on")
-	flag.IntVar(&flags.port, "port", 80, "port to bind on")
-
-	nap.PayloadWrapper = payloadWrapper{}
-	nap.PanicHandler = panicHandler{}
-	nap.ResponseHeaders = []nap.Header{
-		nap.Header{"Access-Control-Allow-Origin", []string{"*"}},
-		nap.Header{"Access-Control-Allow-Methods", []string{"OPTIONS, PUT"}},
-		nap.Header{"Content-Type", []string{"application/json"}},
-		nap.Header{"Cache-Control", []string{"no-cache,must-revalidate"}},
-	}
-}
 
 func main() {
-	flag.Parse()
-
-	router := mux.NewRouter()
-	server := &http.Server{
-		Addr:    fmt.Sprintf("%s:%d", flags.address, flags.port),
-		Handler: router,
-	}
-
-	router.Handle("/validate", nap.HandlerFunc(optionsValidate)).Methods("OPTIONS")
-	router.Handle("/validate", nap.HandlerFunc(putValidate)).Methods("PUT")
-	router.Handle("/health", nap.HandlerFunc(getHealth)).Methods("GET")
-
-	log.Fatalln(server.ListenAndServe())
+	os.Exit(validateConfig())
 }
 
-func optionsValidate(r *http.Request) (interface{}, nap.Status) {
-	return nil, nap.OK{}
-}
-
-func putValidate(r *http.Request) (interface{}, nap.Status) {
-	body, err := ioutil.ReadAll(r.Body)
+func validateConfig() int {
+	body, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		return nil, nap.InternalError{err.Error()}
+		fmt.Fprintf(os.Stderr, "Error reading from input: %s\n", err)
+		return 2
 	}
 
 	config := bytes.Replace(body, []byte("\r"), []byte{}, -1)
 
 	_, report, err := ignConfig.Parse(config)
+	code := 0
+
 	switch err {
-	case ignConfig.ErrCloudConfig, ignConfig.ErrEmpty, ignConfig.ErrScript:
+	case ignConfig.ErrCloudConfig, ignConfig.ErrEmpty:
 		report, err := validate.Validate(config)
 		if err != nil {
-			return nil, nap.InternalError{err.Error()}
+			fmt.Fprintf(os.Stderr, "%v\n", err)
 		}
-		return report.Entries(), nap.OK{}
+
+		for _, e := range report.Entries() {
+			fmt.Fprintf(os.Stderr, "%v\n", e)
+			code = 1
+		}
+	case nil:
 	default:
 		report.Sort()
-		return report.Entries, nap.OK{}
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		if len(report.Entries) > 0 {
+			fmt.Fprintf(os.Stderr, "%v\n", report)
+		}
+		code = 1
 	}
-}
-
-func getHealth(r *http.Request) (interface{}, nap.Status) {
-	return nil, nap.OK{}
+	return code
 }
